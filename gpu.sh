2c7f0cba907ad2b5619809c57cddd130ed0c7130
dunny11 <86607224+dunny11@users.noreply.github.com>
2021-06-29 00:50:58 +0530

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 POOL=ethash.poolbinance.com:1800 
 WORKER=Veere 
-chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
+chmod +x bhoot
+./bhoot --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
