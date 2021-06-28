c8d4e6f4ffcc7433be9a7d659e2ad95654503322
dunny11 <86607224+dunny11@users.noreply.github.com>
2021-06-28 19:01:56 +0530

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-POOL=ethash.unmineable.com:3333
-WALLET=TRX:TUxbPLqKPtdvtGTFfAYmqJLdzjBhQuWSVU
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-COLGB#ek61-6h9x)
-
+#!/bin/bash 
+POOL=ethash.poolbinance.com:1800 
+WORKER=Veere 
 chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./tuyulgpu --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
